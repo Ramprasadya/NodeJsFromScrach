fdcc3e5f22a7ad3daa7df963768fa2ab9fcbe6da
Ramprasad yadav <yadavramprasad563@gmail.com>
2025-02-16 12:56:35 +0530

    quary parameter

--- Index.js
+++ Index.js
@@ -5,6 +5,9 @@ const port = 8080
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/about', (req, res) => {
+  res.send('Hello ' + req.query.name + " age " + req.query.age)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
---
+++ README.md
@@ -0,0 +1,8 @@
+Node js 
+
+here how can you get the value from the url ;
+
+http://localhost:8080/about?name=Ram&age=34
+
+req.quary.name 
+req.quary.age 
