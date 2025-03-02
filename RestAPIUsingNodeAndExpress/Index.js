04d72ac70c7877075f2595c8eea000029a2425ea
Ramprasad yadav <yadavramprasad563@gmail.com>
2025-03-02 20:48:03 +0530

    mongoose conn

--- RestAPIUsingNodeAndExpress/Index.js
+++ RestAPIUsingNodeAndExpress/Index.js
@@ -3,35 +3,59 @@ const app = express();
 const port = 2610;
 const users = require("./MOCK_DATA.json");
 const fs = require("fs");
+const mongoose = require("mongoose");
 // Predefined middleware
 // is used to parse incoming request bodies with application/x-www-form-urlencoded content type.
 app.use(express.urlencoded({ extended: false }));
 
 // Another middleware
-app.use((req,res,next)=>{
-  console.log("hello from middleware 1")
-  req.myName = "ram"
+app.use((req, res, next) => {
+  console.log("hello from middleware 1");
+  req.myName = "ram";
   // res.send({msg:"hello from middleware 1"})
 
-  next()
-})
-app.use((req,res,next)=>{
-  console.log("hello from middleware 2",req.myName)
+  next();
+});
+app.use((req, res, next) => {
+  console.log("hello from middleware 2", req.myName);
 
-  next()
-})
+  next();
+});
 
-app.use((req,res,next)=>{
-  fs.appendFile("./log.txt",`\n ${Date.now()} : ${req.method} : ${req.path}`,(err,data)=>{
-    next()
-  })
-})
+// Connection with mongoose
+mongoose
+  .connect("mongodb://localhost:27017/nodeTest")
+  .then(() => console.log("MongoDb connection success"))
+  .then((err)=> console.log(err))
+
+// schema
+
+const UserSchema = new mongoose.Schema({
+  id: String,
+  first_name: String,
+  last_name: String,
+  email: String,
+  gender: String,
+  job_title: String,
+});
+
+const User = mongoose.model("User", UserSchema);
+
+app.use((req, res, next) => {
+  fs.appendFile(
+    "./log.txt",
+    `\n ${Date.now()} : ${req.method} : ${req.path}`,
+    (err, data) => {
+      next();
+    }
+  );
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-}); 
+});
 
-app.get("/user", (req,  res) => {
+app.get("/user", (req, res) => {
   const html = `
     <ul>
     ${users
@@ -44,12 +68,12 @@ app.get("/user", (req,  res) => {
   res.send(html);
 });
 
-app.get("/users",(req,res)=>{
-  console.log("Hello from users route ", req.myName)
-  res.setHeader("X-Name","Ramprasad") //Custom Headers
+app.get("/users", (req, res) => {
+  console.log("Hello from users route ", req.myName);
+  res.setHeader("X-Name", "Ramprasad"); //Custom Headers
   // Always add X to custom headers
-  return res.json(users)
-})
+  return res.json(users);
+});
 
 app
   .route("/user/:id")
@@ -60,32 +84,37 @@ app
   })
   .patch((req, res) => {
     const { id } = req.params;
-       const {first_name,last_name,job_title,gender,email} = req.body;
+    const { first_name, last_name, job_title, gender, email } = req.body;
     // const { data } = req.body;
     let user = users.find((users) => users.id === Number(id));
-    if(first_name) user.first_name = first_name
-    if(last_name) user.last_name = last_name
-    if(job_title) user.job_title =job_title
-    if(gender) user.gender =gender
-    if(email) user.email =email
-    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
-        res.json({status:"success",id:id})
-    })
+    if (first_name) user.first_name = first_name;
+    if (last_name) user.last_name = last_name;
+    if (job_title) user.job_title = job_title;
+    if (gender) user.gender = gender;
+    if (email) user.email = email;
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+      res.json({ status: "success", id: id });
+    });
   })
   .delete((req, res) => {
-    const {id} = req.params
-     let updated =users.filter((user)=>user.id !==Number(id))
-    fs.writeFile("./MOCK_DATA.json",JSON.stringify(updated),()=>{
-
-        return res.json({ status: "Deleeted " ,id:id })
-    })
+    const { id } = req.params;
+    let updated = users.filter((user) => user.id !== Number(id));
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(updated), () => {
+      return res.json({ status: "Deleeted ", id: id });
+    });
   });
 
 // Creating new user  in json file
 app.post("/api/user", (req, res) => {
   const body = req.body;
-  if(!body.first_name || !body.last_name || !body.email || !body.job_title || !body.gender){
-    res.status(400).send({messsage:"All field are required"})
+  if (
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.job_title ||
+    !body.gender
+  ) {
+    res.status(400).send({ messsage: "All field are required" });
   }
   users.push({ ...body, id: users.length + 1 });
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
